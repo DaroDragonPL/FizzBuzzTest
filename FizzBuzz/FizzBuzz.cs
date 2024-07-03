62b2ddf3d336f12eeaff240661c021924dade93a
Bartosz Michalak <bartoszd.michalak@gmail.com>
2024-07-03 12:33:04 +0100

    Created skeleton FizzBuzz class

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,6 +1,50 @@
 ﻿namespace FizzBuzz;
 
-public class Class1
+public interface IFizzBuzz
 {
+    public bool IsDivisible(int divident);
+}
 
+public abstract class FizzBuzzBase : IFizzBuzz
+{
+    protected int divisor;
+    protected string _replacementString;
+
+    public string replacementString
+    {
+        get { return _replacementString; }
+    }
+
+    public FizzBuzzBase(int divisor, string replacementString)
+    {
+        this.divisor = divisor;
+        this._replacementString = replacementString;
+    }
+    public abstract bool IsDivisible(int divident);
+}
+
+public class Fizz : FizzBuzzBase
+{
+    private const int defaultDivisor = 3;
+    private const string defaultReplacementString = "Fizz";
+
+    public Fizz() : base(defaultDivisor, defaultReplacementString) {}
+
+    public override bool IsDivisible(int divident)
+    {
+        return false;
+    }
+}
+
+public class Buzz : FizzBuzzBase
+{
+    private const int defaultDivisor = 5;
+    private const string defaultReplacementString = "Buzz";
+
+    public Buzz() : base(defaultDivisor, defaultReplacementString) {}
+
+    public override bool IsDivisible(int divident)
+    {
+        return false;
+    }
 }
